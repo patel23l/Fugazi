8e6adb5da26d4e3db1397cf195f87f8776fe0790
nandinipatel <patelnandini26@gmail.com>
2021-01-16 17:37:36 -0500

    swap on login works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
-import logo from './fugazi_logo.png';
 import './App.css';
 import { BrowserRouter, Router, Route, Link, Switch } from "react-router-dom";
-
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Upload from "./pages/Upload";
