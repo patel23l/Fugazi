7f977e8993eec7059671357fe0926b66567c56f6
Jared <janstett9@gmail.com>
2021-01-16 14:31:00 -0500

    user login changes

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Navbar from "react-bootstrap/Navbar";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
+
 
 //HTTP module
 import axios from 'axios';
@@ -12,6 +10,9 @@ export default class Login extends React.Component {
   state = {
     email: '',
     password: ''
+  }
+  validateForm() {
+    return this.email.length > 0 && this.password.length > 0;
   }
 
   handleChange = event => {
@@ -25,18 +26,55 @@ export default class Login extends React.Component {
     const user = {
       email: this.state.email,
       password: this.state.password
-  };
+    };
 
-  axios.post()
-}
+    const headers = {
+      'Content-Type': 'x-www-form-urlencoded',
+      'Authorization': 'Bearer'
+    };
 
+    axios.post('./api/auth/login', { user }, { headers })
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+  }
+  render (){
+    return (
+      <div className="Login">
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={this.email}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={this.password}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" >
+          Login
+        </Button>
+      </Form>
+      </div>
+    )
+    }
+/*
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     function validateForm() {
-        return email.length > 0 && password.length > 0;
-    }
+      return email.length > 0 && password.length > 0;
+  }
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -68,4 +106,5 @@ export default function Login() {
         </Form>
       </div>
     );
+    */
 }
