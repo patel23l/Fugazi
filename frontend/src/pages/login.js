f92feeb1354919b5b86b6e3c2293b37dc2a656eb
Jared <janstett9@gmail.com>
2021-01-16 11:16:37 -0500

    Frontend changes

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,7 +1,7 @@
 import React, {usestate} from "react;"
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap-Button";
-import "./login.css"
+import ".pages/login.css";
 
 export default function Login() {
     const [email, setEmail] = useState("");
---
+++ frontend/src/pages/upload.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+const Upload = () => {
+    return (
+       <div>
+          <h1>Upload</h1>
+          <p>Upload video</p>
+       </div>
+    );
+}
+ 
+export default Upload;
