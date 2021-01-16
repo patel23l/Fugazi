7c2298298e45d055f0102f8f8e889bd1254f4869
Jared <janstett9@gmail.com>
2021-01-16 13:04:36 -0500

    Better navbar

--- frontend/src/pages/Navigation.js
+++ frontend/src/pages/Navigation.js
@@ -1,24 +1,40 @@
 import React from 'react';
- 
+import Navbar from "react-bootstrap/Navbar" ;
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+
 import { NavLink } from 'react-router-dom';
 import logo from './fugazi_logo.png';
 
 const Navigation = () => {
     return (
        <div class="container">
-        <div class="navbar-header responsive-logo">
-            <a href="/" class="navbar-brand"><img src={logo} alt-text="Fugazi Logo"></img></a>
-        </div>
-        <nav class="navbar-collapse bs-navbar-collapse collapse" role="navigation" id="site-navigation">
-			<ul id="menu-header-menu" class="nav navbar-nav navbar-right responsive-nav main-nav-list">
-            <li><NavLink to="/">Login</NavLink></li>
-            <li><NavLink to="/dashboard">Dashboard</NavLink></li> 
-            <li><NavLink to="/upload">Upload</NavLink></li>      
-            </ul>	
-           
-          
-          
-        </nav>
+        <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home"><a href="/" class="navbar-brand"><img src={logo} alt-text="Fugazi Logo"></img></a></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/">Login</Nav.Link>
+      <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+      <Nav.Link href="/upload">Upload</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>`
+
        </div>
     );
 }
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-
+import Navbar from "react-bootstrap/Navbar";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 
 export default function Login() {
     const [email, setEmail] = useState("");
