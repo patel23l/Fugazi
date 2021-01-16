af142a9a7eb9c94d03b60ff6de1f239ee4caab32
Jared <janstett9@gmail.com>
2021-01-16 10:37:06 -0500

    Login cont'd

--- frontend/src/containers/login.js
+++ frontend/src/containers/login.js
@@ -1,6 +1,6 @@
 import React, {usestate} from "react;"
 import Form from "react-bootstrap/Form";
-import Butto from "react-bootstrap-Button";
+import Button from "react-bootstrap-Button";
 import "./login.css"
 
 export default function Login() {
